04c42c7eacece3b6a5a8bc62a318a62586d75def
Jason <jason@Jasons-MacBook-Pro.local>
2014-11-12 11:11:10 -0600

    Removed console from human.

--- lib/human.rb
+++ lib/human.rb
@@ -2,14 +2,11 @@ class Human
 
   attr_reader :token
 
-  def initialize(token, ui)
+  def initialize(token)
     @token = token
-    @ui = ui
   end
 
   def move(board)
-    @ui.draw_board(board)
-    spot = @ui.prompt_for_move
-    board[spot].nil? ? board.place(@token, spot) : move(board)
+    # TODO: Implement or remove this
   end
 end
--- spec/human_spec.rb
+++ spec/human_spec.rb
@@ -4,36 +4,9 @@ require 'board'
 describe 'Human' do
 
   let(:board)   { Board.new }
-  let(:console) { instance_double('Console', {
-    :draw_board => nil,
-    :prompt_for_move => 4
-    }) }
-  let(:human)   { Human.new(:X, console) }
+  let(:human)   { Human.new(:X) }
 
   context '#move' do
-    it 'displays the board' do
-      expect(console).to receive(:draw_board)
-      human.move(board)
-    end
-
-    it 'takes an input from the user.' do
-      expect(console).to receive(:prompt_for_move)
-      human.move(board)
-    end
-
-    it 'makes a move on the board' do
-      human.move(board)
-      expect(board.empty?).to eq(false)
-    end
-
-    it 'will run until the user picks an empty spot on the board' do
-      board.place(human.token, 4)
-      board.place(human.token, 0)
-      board.place(human.token, 1)
-      board.place(human.token, 2)
-      allow(console).to receive(:prompt_for_move).and_return(4, 4, 4, 0, 1, 4, 2, 3)
-      human.move(board)
-      expect(board[3]).to eq(human.token)
-    end
+    # Nothing yet.
   end
 end

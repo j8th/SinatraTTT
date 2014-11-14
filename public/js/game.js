d337ccf2051b4d36619be7e6286ceb8c86ff6b49
Jason <jason@Jasons-MacBook-Pro.local>
2014-11-14 14:53:43 -0600

    Some basic Game spec for listen_board_click_event().

--- public/js/game.js
+++ public/js/game.js
@@ -1,9 +1,17 @@
 'use strict';
 
-function Game() {
+function Game(a_board, a_server) {
+  // Private Data Members
+  var board = a_board;
+  var server = a_server;
+
+  // Initialization
 
 
-  this.listen_board_click_event = function(index) {
-
+  // Public Methods
+  this.listen_board_click_event = function(spot) {
+    server.send_move(spot, function(data){
+      board.place('X', spot);
+    });
   };
 }
--- spec/javascripts/game_spec.js
+++ spec/javascripts/game_spec.js
@@ -1,12 +1,33 @@
 describe('Game', function(){
+  var board;
+  var server;
   var game;
 
+
   beforeEach(function(){
-    game = new Game();
+    board = new Board();
+    server = new Server();
+    game = new Game(board, server);
+
+    // Mock out the Server so we don't make real ajax requests.
+    spyOn(server, 'send_move').and.callFake(function(myspot, success_callback){
+      var data = '0';  // The server returns its move.
+      success_callback(data);
+    });
   });
 
+
+
   describe('#listen_board_click_event', function(){
-    // No tests yet.
+    it('sends a move to the server with the spot clicked.', function(){
+      game.listen_board_click_event(4);
+      expect(server.send_move).toHaveBeenCalled();
+    });
+
+    it('updates the board with the move on success from the server.', function(){
+      game.listen_board_click_event(4);
+      expect(board.get(4)).toEqual('X');
+    });
   });
 
 });

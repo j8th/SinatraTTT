50ba656162db54b553221529a49db0abf8a3194f
Jason <jason@Jasons-MacBook-Pro.local>
2014-12-01 12:51:25 -0600

    Remove some unused code and tests.

--- lib/server.rb
+++ lib/server.rb
@@ -15,25 +15,5 @@ get '/' do
   erb :index, :locals => {:board => board}
 end
 
-get '/game/new' do
-  session[:game] = Game.new(Board.new, Human.new(:X), AI.new(:O))
-end
-
-post '/move' do
-  game = session[:game]
-  spot = params[:spot].to_i
-  game.turn(spot)
-  spot = game.turn()
-  { :aimove => spot }.to_json
-end
-
-get '/next-state' do
-  board = Board::from_s(params[:board])
-  ai = AI.new(:O)
-  ai.move(board)
-  board.to_s
-end
-
-
 
 
